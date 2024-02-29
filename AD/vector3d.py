768caca83d3a8bcbe093297c9be3017d7f3671aa
a-domagalski <a.domagalski96@gmail.com>
2024-02-29 10:59:01 +0100

    z1p2 done

--- AD/vector3d.py
+++ AD/vector3d.py
@@ -1,4 +1,9 @@
 
 class Vector3d:
-    def __init__(x, y, z):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
         
+    def __str__(self):
+        print("Vector3d({}, {}, {})".format(self.x, self.y, self.z))
